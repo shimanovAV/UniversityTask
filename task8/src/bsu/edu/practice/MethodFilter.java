9791aa518b8648327a0095256cb8fb392b40ff96
shimanovAV <aleksandra.shimanovich@mail.ru>
2019-04-23 10:39:38 +0300

    finish task 8

---
+++ task8/src/bsu/edu/practice/MethodFilter.java
@@ -0,0 +1,25 @@
+package bsu.edu.practice;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+public class MethodFilter implements Filter {
+
+    public void init(FilterConfig filterConfig)
+    {}
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        long start = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest,servletResponse);
+        long end =System.currentTimeMillis();
+        long time = end - start;
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        String methodName = req.getMethod();
+        String url = req.getRequestURL().toString();
+        String info = methodName + " " + url + " " + time;
+        System.out.println(info);
+    }
+
+    @Override
+    public void destroy() {}
+}
